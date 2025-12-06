a3de2d893ce74420bc206aea39dc4ed69391209b
ajaydev <ajaydevkomath@gmail.com>
2025-12-06 22:03:25 +0530

    Add route handling for creating and updating routes: implement POST and PUT endpoints in route.routes.js with corresponding controller methods.

--- Server/routes/route.routes.js
+++ Server/routes/route.routes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const routeController = require('../controllers/route.controller');
 
+
+
+router.post('/', routeController.createRoute);
+router.put('/:id', routeController.updateRoute);
+module.exports = router;
